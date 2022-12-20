3e74eb93c51b627c14f089f5690a7b8f583d3197
kimkyuuuuri <rnflrnfl0633@naver.com>
2022-12-20 13:33:31 +0900

    -

--- php/orders.php
+++ php/orders.php
@@ -27,7 +27,7 @@ function selectReservation()
 {
     $pdo = pdoSqlConnect();
     $query = "
-SELECT Order_id,Product_name,Order_datetime,Order_many,Price,Order_number FROM ORDERS O,PRODUCT P,CUSTOMERS C
+SELECT Order_id,Product_name,Order_datetime,Order_many,Price,Order_number,O.address,Order_price FROM ORDERS O,PRODUCT P,CUSTOMERS C
 WHERE O.product_id=P.Product_id and C.Customer_id=O.customer_id and order_or_delivery=0
 and O.customer_id=1";
     $st = $pdo->prepare($query);
@@ -40,13 +40,13 @@ and O.customer_id=1";
 }
 
 //예약 주문하기
-function insertReservation($customer_id,$product_id,$Order_number,$Order_many,$Order_datetime,$order_or_delivery,$reservationTime)
+function insertReservation($customer_id,$product_id,$Order_number,$Order_many,$Order_datetime,$order_or_delivery,$reservationTime,$Order_price)
 {
     $pdo = pdoSqlConnect();
-    $query = "INSERT INTO ORDERS (customer_id,product_id, Order_number, Order_many, Order_datetime,order_or_delivery,reservationTime )
-                VALUES (?,?,?,?,?,?,?)";
+    $query = "INSERT INTO ORDERS (customer_id,product_id, Order_number, Order_many, Order_datetime,order_or_delivery,reservationTime,Order_price )
+                VALUES (?,?,?,?,?,?,?,?)";
     $st = $pdo->prepare($query);
-    $st->execute([$customer_id,$product_id,$Order_number,$Order_many,$Order_datetime,$order_or_delivery,$reservationTime]);
+    $st->execute([$customer_id,$product_id,$Order_number,$Order_many,$Order_datetime,$order_or_delivery,$reservationTime,$Order_price]);
     $st->setFetchMode(PDO::FETCH_ASSOC);
     $result = $st->fetchAll();
     $st = null;
@@ -56,13 +56,13 @@ function insertReservation($customer_id,$product_id,$Order_number,$Order_many,$O
 
 
 //배달 주문하기
-function insertDelivery($customer_id,$product_id,$Order_number,$Order_many,$Order_datetime,$order_or_delivery,$address)
+function insertDelivery($customer_id,$product_id,$Order_number,$Order_many,$Order_datetime,$order_or_delivery,$address, $Order_price)
 {
     $pdo = pdoSqlConnect();
-    $query = "INSERT INTO ORDERS (customer_id,product_id, Order_number, Order_many, Order_datetime,order_or_delivery,address )
-                VALUES (?,?,?,?,?,?,?)";
+    $query = "INSERT INTO ORDERS (customer_id,product_id, Order_number, Order_many, Order_datetime,order_or_delivery,address,Order_price )
+                VALUES (?,?,?,?,?,?,?,?)";
     $st = $pdo->prepare($query);
-    $st->execute([$customer_id,$product_id,$Order_number,$Order_many,$Order_datetime,$order_or_delivery,$address]);
+    $st->execute([$customer_id,$product_id,$Order_number,$Order_many,$Order_datetime,$order_or_delivery,$address,$Order_price]);
     $st->setFetchMode(PDO::FETCH_ASSOC);
     $result = $st->fetchAll();
     $st = null;
@@ -76,7 +76,7 @@ function selectDelivery()
 {
     $pdo = pdoSqlConnect();
     $query = "
-SELECT Order_id,Product_name,Order_datetime,Order_many,Price,Order_number FROM ORDERS O,PRODUCT P,CUSTOMERS C
+SELECT Order_id,Product_name,Order_datetime,Order_many,Price,Order_number,address ,O.status , Order_price FROM ORDERS O,PRODUCT P,CUSTOMERS C
 WHERE O.product_id=P.Product_id and C.Customer_id=O.customer_id and order_or_delivery=1
 and O.customer_id=1";
     $st = $pdo->prepare($query);
@@ -93,7 +93,7 @@ function selectManagerReservation()
 {
     $pdo = pdoSqlConnect();
     $query = "
-SELECT Order_id,Product_name,Order_datetime,Order_many,Price,Order_number FROM ORDERS O,PRODUCT P,CUSTOMERS C
+SELECT Order_id,Product_name,Order_datetime,Order_many,Price,Order_number, O.status,O.address ,Order_price FROM ORDERS O,PRODUCT P,CUSTOMERS C
 WHERE O.product_id=P.Product_id and C.Customer_id=O.customer_id and order_or_delivery=0";
     $st = $pdo->prepare($query);
     $st->execute();
@@ -109,7 +109,7 @@ function selectManagerDelivery()
 {
     $pdo = pdoSqlConnect();
     $query = "
-SELECT Order_id,Product_name,Order_datetime,Order_many,Price,Order_number FROM ORDERS O,PRODUCT P,CUSTOMERS C
+SELECT Order_id,Product_name,Order_datetime,Order_many,Price,Order_number, O.address, O.status ,Order_price FROM ORDERS O,PRODUCT P,CUSTOMERS C
 WHERE O.product_id=P.Product_id and C.Customer_id=O.customer_id and order_or_delivery=1";
     $st = $pdo->prepare($query);
     $st->execute();
@@ -119,6 +119,21 @@ WHERE O.product_id=P.Product_id and C.Customer_id=O.customer_id and order_or_del
     $pdo = null;
     return $result;
 }
+
+//관리자 예약, 배송 취소
+function updateManagerDeliveryStatus($orderId)
+{
+    $pdo = pdoSqlConnect();
+    $query = "UPDATE ORDERS SET status = 'F' WHERE Order_id=?";
+    $st = $pdo->prepare($query);
+    $st->execute([$orderId]);
+    $st->setFetchMode(PDO::FETCH_ASSOC);
+    $result = $st->fetchAll();
+    $st = null;
+    $pdo = null;
+    return $result;
+}
+
 
 
 if($_SERVER["REQUEST_METHOD"]=="GET" ) {
@@ -146,11 +161,19 @@ if($_SERVER["REQUEST_METHOD"]=="GET" ) {
             echo json_encode(array("reservationList"=>$res),JSON_UNESCAPED_UNICODE);
         }
 
+
         //관리자 배송 조회
         if ( $_SERVER['REQUEST_URI']=="/orders.php/selectManagerDelivery"){
 
             $res=selectManagerDelivery();
             echo json_encode(array("deliveryList"=>$res),JSON_UNESCAPED_UNICODE);
+        }
+
+        if (  strpos($_SERVER['REQUEST_URI'],"/orders.php/updateManagerDeliveryStatus")!==false){
+            $res=["성공"];
+            $orderId = $_GET['orderId'];
+            updateManagerDeliveryStatus($orderId);
+            echo json_encode(array("result"=>$res),JSON_UNESCAPED_UNICODE);
         }
 
 
@@ -168,7 +191,7 @@ catch (Exception $e) {
 if($_SERVER["REQUEST_METHOD"]=="POST" ) {
 
     try {
-        //회원가입
+
         $res=["성공"];
         if ( $_SERVER['REQUEST_URI']=="/orders.php/insertReservation"){
             //body값 받는 코드
@@ -179,9 +202,10 @@ if($_SERVER["REQUEST_METHOD"]=="POST" ) {
             $Order_many = $_POST["Order_many"];
             $Order_datetime = $_POST["Order_datetime"];
             $reservationTime= $_POST["reservationTime"];
+            $Order_price= $_POST["Order_price"];
 
             //데이터 넣는 코드
-            insertReservation($customer_id,$product_id,$Order_number,$Order_many,$Order_datetime,0,$reservationTime);
+            insertReservation($customer_id,$product_id,$Order_number,$Order_many,$Order_datetime,0,$reservationTime,$Order_price);
 
 
             //응답 코드
@@ -197,9 +221,10 @@ if($_SERVER["REQUEST_METHOD"]=="POST" ) {
             $Order_many = $_POST["Order_many"];
             $Order_datetime = $_POST["Order_datetime"];
             $address= $_POST["address"];
+            $Order_price= $_POST["Order_price"];
 
             //데이터 넣는 코드
-            insertDelivery($customer_id,$product_id,$Order_number,$Order_many,$Order_datetime,1,$address);
+            insertDelivery($customer_id,$product_id,$Order_number,$Order_many,$Order_datetime,1,$address,$Order_price);
 
             //응답 코드
             echo json_encode(array("result"=>$res),JSON_UNESCAPED_UNICODE);
